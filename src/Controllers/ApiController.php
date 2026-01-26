873df99a28d36789b54de5d8d01b5461d20d6748
lucas-frnd <lucas.frnd@eduge.ch>
2026-01-26 11:38:23 +0100

    upddate

---
+++ src/Controllers/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EvanAlpst\ApiFoot\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use EvanAlpst\ApiFoot\Models\User;
+use EvanAlpst\ApiFoot\Services\UserService;
+
+class ApiController extends BaseController
+{
+    
+}
