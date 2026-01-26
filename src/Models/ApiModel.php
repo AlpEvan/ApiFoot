0fab652494c5ae210986e1c0726d7d134a1c026a
lucas-frnd <lucas.frnd@eduge.ch>
2026-01-26 10:46:10 +0100

    model

--- src/Models/ApiModel.php
+++ src/Models/ApiModel.php
@@ -5,8 +5,27 @@ declare(strict_types=1);
 namespace EvanAlpst\ApiFoot\Models;
 
 use EvanAlpst\ApiFoot\Controller\ApiController;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class ApiModel
 {
+    private Client $client;
+    private string $apiKey;
+    private string $baseUrl;
     
+    public function __construct(string $apiKey, string $baseUrl) 
+    {
+        $this->apiKey = $apiKey;
+        $this->baseUrl = $baseUrl;
+        
+        $this->client = new Client([
+            'base_uri' => $baseUrl,
+            'timeout' => 10.0,
+            'headers' => [
+                'X-API-Key' => $apiKey,
+                'Content-Type' => 'application/json'
+            ]
+        ]);
+    }
 }
